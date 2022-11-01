b5cf85c650e0f80effa47e57edffb8c1161ef82d
Daniel-Kigozi <kdanalex69@gmail.com>
2022-11-01 21:56:41 +0300

    add markup and javascript to todo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
-import _ from 'lodash';
-import './style.css';
 
-function component() {
-    const element = document.createElement('div');
+import './style.css';
+const divContainer = document.querySelector(".books-container");
+
+
+
+const todoList = [
+    {
+      description: 'Go shopping for lunch',
+      completed: false,
+      index: 0,
+    },
+    {
+      description: 'Take kids to school',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Work on coding challenges',
+      completed: false,
+      index: 2,
+    },
+    {
+        description: 'Go to the gym',
+        completed: false,
+        index: 3,
+      },
+    {
+      description: 'Attend workshop',
+      completed: false,
+      index: 4,
+    },
+  ];
+
+  const displayTodo = ({ description, index }) => {
+    const divElement = document.createElement('div');
+    divElement.className = 'first-item';
+    divElement.innerHTML = `
+      <div class="item-details">
+      <input type="checkbox" id="" name="" value=""> <h4 class="item-desription">${description}</h4>
+      </div>
+      <i class="fa-solid fa-trash-can" id"${index}"></i>
+      `;
   
-    
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    return element;
-  }
+    return divElement;
+  };
   
-  document.body.appendChild(component());
+  todoList.forEach((todo) => {
+    divContainer.append(displayTodo(todo));
+  });
   
