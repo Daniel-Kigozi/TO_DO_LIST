b99c285385f6fb1fd8d00bd9067208f352976b3d
Daniel-Kigozi <kdanalex69@gmail.com>
2022-11-04 15:55:28 +0300

    add js linter

--- src/complete.js
+++ src/complete.js
@@ -1,31 +1,27 @@
-
-
 let todoList = [];
 
+const setLocalStorage = (todoList) => {
+  localStorage.setItem('formInputs', JSON.stringify(todoList));
+};
 
-const mainToggle = ()=>{
-    const togle = document.querySelectorAll('.complete');
-  togle.forEach((toggleItem, index)=> {
-    toggleItem.addEventListener('change', ()=>{
-      let todoList = getLocalStorage();
-      todoList[index].completed= todoList[index].completed ? false : true;
+const getLocalStorage = () => {
+  if (localStorage.getItem('formInputs') !== null) {
+    todoList = JSON.parse(localStorage.getItem('formInputs'));
+  } else {
+    todoList = [];
+  }
+  return todoList;
+};
+
+const mainToggle = () => {
+  const togle = document.querySelectorAll('.complete');
+  togle.forEach((toggleItem, index) => {
+    toggleItem.addEventListener('change', () => {
+      const todoList = getLocalStorage();
+      todoList[index].completed = !todoList[index].completed;
       setLocalStorage(todoList);
-    })
-  })
-  }
-  
-const setLocalStorage = (todoList)=>{
-    localStorage.setItem('formInputs', JSON.stringify(todoList));
-  }
+    });
+  });
+};
 
-const getLocalStorage = ()=>{
-    if (localStorage.getItem('formInputs') !== null) {
-      todoList = JSON.parse(localStorage.getItem('formInputs'));
-      } else {
-      todoList = [];
-      }
-      return todoList;
-  }
-
-
-  export { mainToggle, setLocalStorage, getLocalStorage}
+export { mainToggle, setLocalStorage, getLocalStorage };
--- src/index.js
+++ src/index.js
@@ -1,100 +1,25 @@
-
-  import { getLocalStorage, mainToggle, setLocalStorage } from './complete.js';
+import { getLocalStorage, mainToggle, setLocalStorage } from './complete.js';
 import './style.css';
-  const divContainer = document.querySelector(".books-container");
-  const inputDiv = document.querySelector("#title");
-  const inputSubmit = document.querySelector("#submit");
-  
-  let todoList = getLocalStorage() || [];
-  
-  let isEdit = false
-  let editId = null;
-  mainToggle();
 
-  
-  // const setLocalStorage = (todoList)=>{
-  //   localStorage.setItem('formInputs', JSON.stringify(todoList));
-  // }
-  
-  // const getLocalStorage = ()=>{
-  //   if (localStorage.getItem('formInputs') !== null) {
-  //     todoList = JSON.parse(localStorage.getItem('formInputs'));
-  //     } else {
-  //     todoList = [];
-  //     }
-  //     return todoList;
-  // }
-  setLocalStorage(todoList); 
-  getLocalStorage();
+const divContainer = document.querySelector('.books-container');
+const inputDiv = document.querySelector('#title');
+const inputSubmit = document.querySelector('#submit');
 
-  const reassignIndex = (todoList) => {
-    todoList.forEach((item, i) => {
-      item.index = i + 1;
-    });
-  }
+let todoList = getLocalStorage() || [];
 
-  
-  
-    const deleteTask = (id) => {
-      let todoList = getLocalStorage();
-      const taskToDelete = todoList[id];
-    
-      todoList = todoList.filter((item) => item !== taskToDelete);
-      reassignIndex(todoList)
-    
-      setLocalStorage(todoList);
-  
-      divContainer.innerHTML='';
-      todoList.forEach((item)=>{
-        divContainer.append(displayTodo(item));
-        
-      })
-    
-      removeEvents()
-      editEvents()
-    };
-  
-  
-    const removeEvents = ()=>{
-      document.querySelectorAll('.delete').forEach((button)=> button.addEventListener('click', (e)=>{
-        e.preventDefault();
-        let id;
-        if (button.id > 0) {
-          id = button.id - 1;
-        } else {
-          id = 0;
-        }
-  
-        deleteTask(id)
-      }))
-    }
-  
-    const editEvents = ()=>{
-      document.querySelectorAll('.edit').forEach((button)=> button.addEventListener('click', (e)=>{
-        e.preventDefault();
-        let id;
-        if (button.id > 0) {
-          id = button.id - 1;
-        } else {
-          id = 0;
-        }
-  
-        isEdit = true;
-        editId =id
-  
-        inputDiv.value = todoList[id].description
-        inputDiv.focus();
-  
-      }))
-    }
-  
-    const displayTodo = ({ description, index, completed}) => {
-      
-        const divElement = document.createElement('div');
-        divElement.className = 'first-item';
-        divElement.innerHTML = `
+let isEdit = false;
+let editId = null;
+mainToggle();
+
+setLocalStorage(todoList);
+getLocalStorage();
+
+const displayTodo = ({ description, index, completed }) => {
+  const divElement = document.createElement('div');
+  divElement.className = 'first-item';
+  divElement.innerHTML = `
         <div class="item-details">
-        <input type="checkbox" name="" value="" class="complete" ${completed? 'checked':''}> <h4 class="item-desription">${description}</h4>
+        <input type="checkbox" name="" value="" class="complete" ${completed ? 'checked' : ''}> <h4 class="item-desription">${description}</h4>
         
         <i class="fa-solid fa-trash-can" id"${index}"></i>
         <div class="button-details">
@@ -103,81 +28,119 @@ import './style.css';
         </div>
         </div>
               `;
-    
-      return divElement;
-      
-    };
-  
-    inputSubmit.addEventListener('click', (e) => {
-      e.preventDefault();
-        if(inputDiv.value.trim()!== ''){
-          if(isEdit === false) {
-                      const list = {
-              description: inputDiv.value,
-              completed: false,
-              index: todoList.length +1, 
-          };
 
-          todoList.push(list);
-          setLocalStorage(todoList);
-          todoList = getLocalStorage();
-          divContainer.innerHTML='';setLocalStorage(todoList);
-          todoList.forEach((item)=>{
-            divContainer.append(displayTodo(item));
-          })
-          } else {
-            todoList[editId].description = inputDiv.value
-            setLocalStorage(todoList);
-            todoList = getLocalStorage();
-            divContainer.innerHTML='';
-            todoList.forEach((item)=>{
-              divContainer.append(displayTodo(item));
-            })
-            isEdit = false;
-            
-          }
-          mainToggle();
-          inputDiv.value = ``;
-          removeEvents();
-         editEvents();
-         
-          }; 
-    
-    });
-  
-    
-    todoList = getLocalStorage();
-    divContainer.innerHTML='';
-    todoList.forEach((item)=>{
-      divContainer.append(displayTodo(item));
+  return divElement;
+};
+const editEvents = () => {
+  document.querySelectorAll('.edit').forEach((button) => button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let id;
+    if (button.id > 0) {
+      id = button.id - 1;
+    } else {
+      id = 0;
+    }
 
-    
-    })
-    mainToggle()
-    removeEvents()
-    editEvents() 
+    isEdit = true;
+    editId = id;
 
-    const deleteAllItems = document.querySelector('.clear-data');
-    deleteAllItems.addEventListener('click', ()=>{
-      todoList = getLocalStorage();
-      todoList = todoList.filter((item)=>{
-        return item.completed===false;
-      }) 
+    inputDiv.value = todoList[id].description;
+    inputDiv.focus();
+  }));
+};
+
+const reassignIndex = (todoList) => {
+  todoList.forEach((item, i) => {
+    item.index = i + 1;
+  });
+};
+
+const removeEvents = () => {
+  document.querySelectorAll('.delete').forEach((button) => button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let id;
+    if (button.id > 0) {
+      id = button.id - 1;
+    } else {
+      id = 0;
+    }
+/* eslint-disable */
+    deleteTask(id);
+    
+  }));
+};
+
+const deleteTask = (id) => {
+  let todoList = getLocalStorage();
+  const taskToDelete = todoList[id];
+
+  todoList = todoList.filter((item) => item !== taskToDelete);
+  reassignIndex(todoList);
+
+  setLocalStorage(todoList);
+
+  divContainer.innerHTML = '';
+  todoList.forEach((item) => {
+    divContainer.append(displayTodo(item));
+  });
+
+  removeEvents();
+  editEvents();
+};
+
+inputSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (inputDiv.value.trim() !== '') {
+    if (isEdit === false) {
+      const list = {
+        description: inputDiv.value,
+        completed: false,
+        index: todoList.length + 1,
+      };
+
+      todoList.push(list);
       setLocalStorage(todoList);
-      divContainer.innerHTML='';
-      todoList.forEach((item)=>{
-      divContainer.append(displayTodo(item));
+      todoList = getLocalStorage();
+      divContainer.innerHTML = ''; setLocalStorage(todoList);
+      todoList.forEach((item) => {
+        divContainer.append(displayTodo(item));
+      });
+    } else {
+      todoList[editId].description = inputDiv.value;
+      setLocalStorage(todoList);
+      todoList = getLocalStorage();
+      divContainer.innerHTML = '';
+      todoList.forEach((item) => {
+        divContainer.append(displayTodo(item));
+      });
+      isEdit = false;
+    }
+    mainToggle();
+    inputDiv.value = '';
+    removeEvents();
+    editEvents();
+  }
+});
 
-    
-    })
-      mainToggle(); 
-      removeEvents();
-      editEvents();
-      
-    })
-    
-  
-    
-    
+todoList = getLocalStorage();
+divContainer.innerHTML = '';
+todoList.forEach((item) => {
+  divContainer.append(displayTodo(item));
+});
+mainToggle();
+removeEvents();
+editEvents();
 
-    
+const deleteAllItems = document.querySelector('.clear-data');
+deleteAllItems.addEventListener('click', () => {
+  todoList = getLocalStorage();
+  todoList = todoList.filter((item) => item.completed === false);
+  setLocalStorage(todoList);
+  divContainer.innerHTML = '';
+  todoList.forEach((item) => {
+    divContainer.append(displayTodo(item));
+  });
+  mainToggle();
+  removeEvents();
+  editEvents();
+});
